c967ae81fcd168509bea08e5fb75ef7c3bf5bbe0
JMaza <maj.jza731@gmail.com>
2026-03-07 14:31:19 -0600

    chore: workspace cleanup - removed obsolete and temporary files from root

--- fix3.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('src/tests/gschema.regression.test.ts', 'utf8');
-c = c.replace(/toBeLessThan\(5000\)/g, 'toBeLessThan(15000)');
-fs.writeFileSync('src/tests/gschema.regression.test.ts', c);
